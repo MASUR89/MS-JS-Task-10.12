50094e166769f041a9c95f9e4b9cebda2868718f
MASUR89 <surmava9@gmail.com>
2024-12-11 20:35:47 +0400

    3 done

---
+++ script.js
@@ -0,0 +1,34 @@
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(post => {
+        console.log(`Title: ${post.title}`);
+     });
+  })
+  .catch(error => console.error("Error fetching posts:", error));
+
+
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: 'Post', 
+    headers: {
+      'Content-Type': 'application/json', 
+    },
+    body: JSON.stringify({
+        userId: "N101", 
+        title: "New Post", 
+        body: "Some content", 
+    }),
+})
+    .then(response => response.json())
+    .then(data => console.log("New Post:", data))
+    .catch(error => console.log(error));
+  
+
+    
+fetch("https://jsonplaceholder.typicode.com/posts/:id", {
+    method: 'Delete',
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
